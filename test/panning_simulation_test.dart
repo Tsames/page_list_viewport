a36447315615f5b0742051abbca027fc61b978b7
Tom Ames <tsames1@gmail.com>
2023-12-11 17:27:08 -0800

    created panning test for stylus. The test currently fails, successfully demonstrating that passing stylus as a device kind to PageListViewportGestures does not work.

--- test/panning_simulation_test.dart
+++ test/panning_simulation_test.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:page_list_viewport/page_list_viewport.dart';
@@ -22,6 +24,52 @@ void main() {
       expect(latestVelocity, Offset.zero);
     });
   });
+  group("Stylus functionality", () {
+    testWidgets("stops panning viewport", (widgetTester) async {
+      final controller = PageListViewportController(vsync: widgetTester);
+      await _pumpPageListViewport(widgetTester, controller: controller);
+      Offset? latestVelocity;
+      controller.addListener(() {
+        latestVelocity = controller.velocity;
+      });
+
+      // Fling up, to scroll down, and run a panning simulation.
+      await widgetTester.fling(find.byType(Scaffold), const Offset(0, -500), 4000);
+      // The viewport is moving
+      widgetTester.pump;
+      expect(latestVelocity == Offset.zero, false);
+
+      await widgetTester.startGesture(const Offset(0, 0), kind: PointerDeviceKind.stylus);
+      // Pumping one frame does not suffice to propagate the stylus gesture
+      // and stop the scrolling simulation, so we're using a pumping duration.
+      while (latestVelocity != Offset.zero) {
+        await widgetTester.pump(const Duration(milliseconds: 100));
+      }
+      expect(latestVelocity == Offset.zero, true);
+    });
+
+    testWidgets("operates as a panning device", (widgetTester) async {
+      final controller = PageListViewportController(vsync: widgetTester);
+
+      //Add stylus to PageListViewPortGestures
+      final deviceKind = {
+        PointerDeviceKind.stylus,
+      };
+      await _pumpPageListViewport(widgetTester, controller: controller, pointerDevices: deviceKind);
+
+      Offset? latestVelocity;
+      controller.addListener(() {
+        latestVelocity = controller.velocity;
+      });
+
+      // Fling up with stylus
+      await widgetTester.fling(find.byType(Scaffold), const Offset(0, -500), 4000, deviceKind: PointerDeviceKind.stylus);
+
+      // The viewport is moving after fling
+      widgetTester.pump;
+      expect(latestVelocity != Offset.zero, false);
+    });
+  });
 }
 
 Future<void> _pumpPageListViewport(
@@ -30,6 +78,7 @@ Future<void> _pumpPageListViewport(
   int pageCount = 10,
   Size? naturalPageSize,
   PageBuilder? pageBuilder,
+  Set<PointerDeviceKind>? pointerDevices,
 }) async {
   controller ??= PageListViewportController(vsync: tester);
   naturalPageSize ??= const Size(8.5, 11) * 72;
@@ -40,6 +89,12 @@ Future<void> _pumpPageListViewport(
       home: Scaffold(
         body: PageListViewportGestures(
           controller: controller,
+          panAndZoomPointerDevices: pointerDevices ??
+              const {
+                PointerDeviceKind.mouse,
+                PointerDeviceKind.trackpad,
+                PointerDeviceKind.touch,
+              },
           child: PageListViewport(
             controller: controller,
             pageCount: pageCount,
